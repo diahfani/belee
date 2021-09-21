0585f8a820739aa7c13024a3fe89444b24e50fca
diahfani <diahaufaarini5@gmail.com>
2021-09-21 14:11:19 +0700

    add routes owners, products and warung

--- routes/route.go
+++ routes/route.go
@@ -8,10 +8,37 @@ import (
 
 func NewRoutes() *echo.Echo {
 	e := echo.New()
-	ev1 := e.Group("api/v1/")
-	ev1.GET("buyers", controllers.GetBuyersController)
-	ev1.GET("buyers/login", controllers.LoginController)
-	ev1.POST("buyers/register", controllers.RegisterController)
-	ev1.GET("buyers/:buyersId", controllers.DetailsBuyers)
+	// ev1 := e.Group("api/v1/")
+
+	//buyers
+	buyers := e.Group("api/v1/buyers")
+	buyers.GET("", controllers.GetBuyersController)
+	buyers.POST("/login", controllers.LoginController)
+	buyers.POST("/register", controllers.RegisterController)
+	buyers.GET("/:buyersId", controllers.DetailsBuyers)
+
+	//owners
+	owners := e.Group("api/v1/owners")
+	owners.GET("", controllers.GetOwnersController)
+	owners.POST("/login", controllers.OwnersLoginController)
+	owners.POST("/register", controllers.OwnersRegisterController)
+	owners.GET("/:buyersId", controllers.DetailsOwners)
+
+	//products
+	products := e.Group("api/v1/products")
+	products.GET("", controllers.GetProducts)
+	products.POST("/add", controllers.CreateProducts)
+	products.GET("/:productsId", controllers.DetailsProducts)
+	products.PUT("/update", controllers.UpdateProducts)
+	products.DELETE("/delete/:productsId", controllers.DeleteProducts)
+
+	//warungs
+	warung := e.Group("api/v1/warungs")
+	warung.GET("", controllers.GetWarung)
+	warung.POST("/add", controllers.AddWarung)
+	warung.GET("/:warungId", controllers.GetDetailsWarung)
+	warung.PUT("/update", controllers.UpdateWarung)
+	warung.DELETE("/delete/:warungId", controllers.DeleteWarung)
+
 	return e
 }
