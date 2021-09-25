5e4d2bf4195087c59ddda9f5be3b915bfc10c074
diahfani <diahaufaarini5@gmail.com>
2021-09-25 19:45:19 +0700

    add config and main.go

--- app/main.go
+++ app/main.go
@@ -1,14 +1,23 @@
-package app
+package main
 
 import (
+	"final_project/belee/app/routes"
+	_buyerUsecase "final_project/belee/business/buyers"
+	_buyerController "final_project/belee/controllers/buyers"
+	_buyerRepository "final_project/belee/drivers/databases/buyers"
+	_buyersdb "final_project/belee/drivers/databases/buyers"
+	_mysqlDriver "final_project/belee/drivers/databases/mysql"
 	"log"
+	"time"
 
+	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
+	"gorm.io/gorm"
 )
 
 //fetch config.json
 func init() {
-	viper.SetConfigFile(`app/config.json`)
+	viper.SetConfigFile(`config.json`)
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
@@ -16,4 +25,38 @@ func init() {
 	if viper.GetBool(`debug`) {
 		log.Println("Service RUN on DEBUG mode")
 	}
+}
+
+func DbMigrate(db *gorm.DB) {
+	db.AutoMigrate(&_buyersdb.Buyers{})
+}
+
+func main() {
+	// init koneksi database
+	configDb := _mysqlDriver.ConfigDB{
+		DB_Username: viper.GetString(`database.user`),
+		DB_Password: viper.GetString(`database.pass`),
+		DB_Host:     viper.GetString(`database.host`),
+		DB_Port:     viper.GetString(`database.port`),
+		DB_Database: viper.GetString(`database.name`),
+	}
+
+	Conn := configDb.InitialDB()
+
+	DbMigrate(Conn)
+
+	e := echo.New()
+	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
+
+	buyerRepository := _buyerRepository.NewMysqlBuyerRepository(Conn)
+	buyerUsecase := _buyerUsecase.NewBuyerUsecase(buyerRepository, timeoutContext)
+	buyerController := _buyerController.NewBuyerController(buyerUsecase)
+
+	routesInit := routes.ControllerList{
+		BuyerController: *buyerController,
+	}
+
+	routesInit.RouteRegister(e)
+	log.Fatal(e.Start(viper.GetString("server.address")))
+
 }
