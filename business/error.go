0e32df86618da7b9f0e9ef1b3a0f99d8196ce8f2
diahfani <diahaufaarini5@gmail.com>
2021-09-28 21:40:38 +0700

    add error

---
+++ business/error.go
@@ -0,0 +1,31 @@
+package business
+
+import "errors"
+
+var (
+	ErrInternalServer = errors.New("something gone wrong")
+
+	ErrNotFound = errors.New("data not found")
+
+	ErrIDNotFound = errors.New("id not found")
+
+	ErrProductIDResource = errors.New("(ProductID) not found or empty")
+
+	ErrProductCodeResource = errors.New("(ProductCode) not found or empty")
+
+	ErrBrandNotFound = errors.New("brand not found")
+
+	ErrDuplicateData = errors.New("duplicate data")
+
+	ErrPhonePasswordNotFound = errors.New("(Phone) or (Password) empty")
+
+	ErrInvalidCredential = errors.New("invalid credential")
+
+	ErrForbiddenRoles = errors.New("forbidden roles")
+
+	ErrMissingID = errors.New("missing required id")
+
+	ErrParamID = errors.New("invalid parameter id")
+
+	ErrAdminKey = errors.New("invalid admin key")
+)
