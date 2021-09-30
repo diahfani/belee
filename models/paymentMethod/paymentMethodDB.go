55de9f9b8ec5fc000206197b9cd06d73ee8a7f13
diahfani <diahaufaarini5@gmail.com>
2021-09-30 14:25:34 +0700

    fixing duplicate name

--- models/paymentMethod/paymentMethodDB.go
+++ models/paymentMethod/paymentMethodDB.go
@@ -4,7 +4,7 @@ import "time"
 
 type PaymentMethods struct {
 	Id        int       `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name" gorm:"unique"`
+	Name      string    `json:"name" gorm:"unique;not null"`
 	CreatedAt time.Time `json:"createdat"`
 	UpdatedAt time.Time
 }
