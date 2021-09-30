f6505c0edae6300500cb83368fbb58f7648fad91
diahfani <diahaufaarini5@gmail.com>
2021-09-30 14:06:26 +0700

    edit dikit

--- models/paymentMethod/paymentMethodDB.go
+++ models/paymentMethod/paymentMethodDB.go
@@ -4,7 +4,7 @@ import "time"
 
 type PaymentMethods struct {
 	Id        int       `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name"`
+	Name      string    `json:"name" gorm:"unique"`
 	CreatedAt time.Time `json:"createdat"`
 	UpdatedAt time.Time
 }
